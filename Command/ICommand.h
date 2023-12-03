df5105c9a8167fab51781765e2e8154508648831
翼 茂木 <k022g0130@m.neec.ac.jp>
2023-12-04 00:50:14 +0900

    1

---
+++ Command/ICommand.h
@@ -0,0 +1,18 @@
+#pragma once
+#include "Object/Player/Player.h"
+
+class Player;
+
+class ICommand {
+public:
+
+	virtual void Exec(Player& player) = 0;
+
+	virtual ~ICommand();
+
+
+private:
+
+
+
+};
